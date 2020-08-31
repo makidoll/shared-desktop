b91696d0f42bb5af5af8379742b3346dc6a42f67
Maki <mxmcube@gmail.com>
2020-08-31 16:53:16 +0100

    Updated stream.sh

--- config/stream.sh
+++ config/stream.sh
@@ -2,7 +2,7 @@ gst-launch-1.0 \
   ximagesrc show-pointer=1 use-damage=0 ! \
     videoscale ! videorate ! video/x-raw,width=1280,height=720,framerate=30/1 ! \
     videoconvert ! queue ! \
-    x264enc pass=cbr bitrate=4000 bframes=0 key-int-max=60 byte-stream=true tune=zerolatency speed-preset=veryfast ! \
+    x264enc pass=cbr bitrate=4000 key-int-max=10 tune=zerolatency speed-preset=veryfast ! \
     video/x-h264,profile=baseline ! \
       rtph264pay pt=96 config-interval=1 ! queue ! \
       udpsink host=127.0.0.1 port=5004 \
