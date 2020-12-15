10239e4c032e0ab2112cd1d44dd2593424d08d13
Maki <mxmcube@gmail.com>
2020-12-15 20:26:36 +0000

    Moved queue around in stream shell scripts

--- config/stream-h264.sh
+++ config/stream-h264.sh
@@ -1,16 +1,16 @@
 gst-launch-1.0 \
-  ximagesrc show-pointer=1 use-damage=0 ! \
+  ximagesrc show-pointer=1 use-damage=0 ! queue ! \
     videoscale ! videorate ! video/x-raw,width=1280,height=720,framerate=30/1 ! \
     videoconvert ! queue ! \
-    x264enc pass=cbr bitrate=4000 key-int-max=10 tune=zerolatency speed-preset=veryfast ! \
-    video/x-h264,profile=baseline ! \
-      rtph264pay pt=96 config-interval=1 ! queue ! \
-      udpsink host=127.0.0.1 port=5004 \
-  pulsesrc device=0 volume=1 ! \
+      x264enc pass=cbr bitrate=4000 key-int-max=10 tune=zerolatency speed-preset=veryfast ! \
+      video/x-h264,profile=baseline ! queue ! \
+        rtph264pay pt=96 config-interval=1 ! \
+        udpsink host=127.0.0.1 port=5004 \
+  pulsesrc device=0 volume=1 ! queue ! \
     audioconvert ! audioresample ! queue ! \
-    opusenc bitrate=48000 audio-type=generic bandwidth=fullband ! \
-      rtpopuspay pt=111 ! queue ! \
-      udpsink host=127.0.0.1 port=5002
+      opusenc bitrate=48000 audio-type=generic bandwidth=fullband ! queue ! \
+        rtpopuspay pt=111 ! \
+        udpsink host=127.0.0.1 port=5002
 
     # nvh264enc bitrate=4000 rc-mode=cbr preset=low-latency ! \
       
--- config/stream-vp8.sh
+++ config/stream-vp8.sh
@@ -1,15 +1,15 @@
 gst-launch-1.0 \
-  ximagesrc show-pointer=1 use-damage=0 ! \
+  ximagesrc show-pointer=1 use-damage=0 ! queue !  \
     videoscale ! videorate ! video/x-raw,width=1280,height=720,framerate=30/1 ! \
     videoconvert ! queue ! \
-    vp8enc target-bitrate=1600000 cpu-used=8 threads=4 deadline=1 error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true ! \
-      rtpvp8pay pt=100 ! queue ! \
-      udpsink host=127.0.0.1 port=5004 \
-  pulsesrc device=0 volume=1 ! \
+      vp8enc target-bitrate=1600000 cpu-used=8 threads=4 deadline=1 error-resilient=partitions keyframe-max-dist=10 auto-alt-ref=true ! queue ! \
+        rtpvp8pay pt=100 ! \
+        udpsink host=127.0.0.1 port=5004 \
+  pulsesrc device=0 volume=1 ! queue ! \
     audioconvert ! audioresample ! queue ! \
-    opusenc bitrate=48000 audio-type=generic bandwidth=fullband ! \
-      rtpopuspay pt=111 ! queue ! \
-      udpsink host=127.0.0.1 port=5002
+      opusenc bitrate=48000 audio-type=generic bandwidth=fullband ! queue ! \
+        rtpopuspay pt=111 ! \
+        udpsink host=127.0.0.1 port=5002
 
     # vp8enc cpu-used=8 threads=4 deadline=1 error-resilient=partitions keyframe-max-dist=20 auto-alt-ref=true ! \
     # vp8enc target-bitrate=800000 cpu-used=8 threads=4 deadline=1 error-resilient=partitions keyframe-max-dist=20 auto-alt-ref=true ! \
