d33dee814d6f5f3c7dce4bcd47d28805fb96c058
Maki <mxmcube@gmail.com>
2020-12-18 03:01:35 +0000

    Added STREAM_BITRATE

--- config/stream-h264-nvenc.sh
+++ config/stream-h264-nvenc.sh
@@ -2,7 +2,7 @@ gst-launch-1.0 \
   ximagesrc show-pointer=1 use-damage=0 ! \
     videoscale ! videorate ! videoconvert ! \
     video/x-raw,width=$STREAM_WIDTH,height=$STREAM_HEIGHT,framerate=30/1,format=NV12 ! queue ! \
-      nvh264enc bitrate=5000 rc-mode=cbr-ld-hq preset=low-latency-hq zerolatency=true ! \
+      nvh264enc bitrate=$STREAM_BITRATE rc-mode=cbr-ld-hq preset=low-latency-hq zerolatency=true ! \
       video/x-h264,profile=baseline ! queue ! \
         rtph264pay pt=96 config-interval=1 ! \
         udpsink host=127.0.0.1 port=5004 \
--- config/stream-h264.sh
+++ config/stream-h264.sh
@@ -2,7 +2,7 @@ gst-launch-1.0 \
   ximagesrc show-pointer=1 use-damage=0 ! \
     videoscale ! videorate ! videoconvert ! \
     video/x-raw,width=$STREAM_WIDTH,height=$STREAM_HEIGHT,framerate=30/1 ! queue ! \
-      x264enc pass=cbr bitrate=4000 key-int-max=10 tune=zerolatency speed-preset=veryfast ! \
+      x264enc pass=cbr bitrate=$STREAM_BITRATE key-int-max=10 tune=zerolatency speed-preset=veryfast ! \
       video/x-h264,profile=baseline ! queue ! \
         rtph264pay pt=96 config-interval=1 ! \
         udpsink host=127.0.0.1 port=5004 \
